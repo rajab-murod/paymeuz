466539c207a2de8f359e0f1942015bb07c58069d
rajab-murod <53085103+rajab-murod@users.noreply.github.com>
2020-08-25 19:51:58 +0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="paymeuz",
+    version="0.1",
+    author="rajab-murod",
+    author_email="rajab.murod1995@gmail.com",
+    description="paymeuz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.5",
+    install_requires=['requests', 'django', 'djangorestframework'],
+    url="https://github.com/rajab-murod/paymeuz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
